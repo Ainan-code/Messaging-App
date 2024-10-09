e88ad1b2b804d0af8f1357cfe74dee28de898020
AINAN HASSAN <ainanhassan123@gmail.com>
2024-10-09 17:00:17 +0300

    message controller update

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -6,7 +6,7 @@ import protectRoute from "../middleware/protectRoute.js";
 const router = express.Router();
 
 
-router.post('/send/:id',protectRoute, sendMessage);
+router.post('/send/:id', protectRoute , sendMessage);
 
 
 
