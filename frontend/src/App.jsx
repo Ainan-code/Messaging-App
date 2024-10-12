227fc4d00389ed779e6350aeabad9c2ffdf0e990
Ainan-code <ainanhassan123@gmail.com>
2024-10-12 23:00:10 +0300

    working on app UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import Login from './Login/Login';
-import Signup from './Signup/Signup';
+
 import './App.css'
+import Home from './pages/Home/Home';
 
 function App() {
  
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-    <Signup/>
+    <Home/>
     </div>
   )
 }
