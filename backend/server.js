28c129ce9a3fa1bceb06b5b3f2425d8a94eed9fb
Ainan-code <ainanhassan123@gmail.com>
2024-10-19 20:42:12 +0300

    prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
  import express from "express";
+ import path from "path";
  import dotenv from "dotenv";
  import cookieParser from "cookie-parser";
  import bodyParser from "body-parser";
@@ -16,6 +17,8 @@
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ extended: false })); // allows to get the payload from request.body
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -26,6 +29,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
  
 
